bd3c00022598890724a658451d9d9780c5a0e1ca
Annie Lee <leeyun@amazon.com>
2021-10-13 18:48:07 -0700

    Merge branch 'dataFilter-fix' into transform-bug-fix

--- public/pages/CreateTransform/components/TransformIndices/TransformIndices.tsx
+++ public/pages/CreateTransform/components/TransformIndices/TransformIndices.tsx
@@ -55,7 +55,6 @@ interface TransformIndicesState {
   targetIndexOptions: { label: string; value?: IndexItem }[];
   isPopoverOpen: boolean;
   selectFieldValue: string;
-  dataFilters: string;
 }
 
 export default class TransformIndices extends Component<TransformIndicesProps, TransformIndicesState> {
@@ -68,7 +67,6 @@ export default class TransformIndices extends Component<TransformIndicesProps, T
       targetIndexOptions: [],
       isPopoverOpen: false,
       selectFieldValue: "",
-      dataFilters: "",
     };
 
     this.onIndexSearchChange = _.debounce(this.onIndexSearchChange, 500, { leading: true });
